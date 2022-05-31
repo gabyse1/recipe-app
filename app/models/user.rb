3ee57d83cf2c5c5d989c52e341aed4c02fc09fef
gabyse1 <gaby.es.iaa@gmail.com>
2022-05-31 08:01:53 -0500

    Create User model associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :foods, class_name: 'Food', foreign_key: 'user_id', inverse_of: :user, dependent: :destroy
+  has_many :recipes, class_name: 'Recipe', foreign_key: 'user_id', inverse_of: :user, dependent: :destroy
+  has_many :inventories, class_name: 'Inventory', foreign_key: 'user_id', inverse_of: :user, dependent: :destroy
 end
