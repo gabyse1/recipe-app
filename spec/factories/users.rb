ad0d361c63debb7b2c7da6452557b32e24997f0c
gabyse1 <gaby.es.iaa@gmail.com>
2022-05-31 10:57:04 -0500

    Create User model spec tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,7 +4,6 @@ FactoryBot.define do
     name { 'test user' }
     email { 'test.user@gmail.com' }
     password { 'password' }
-    password_confirmation { 'password' }
     confirmed_at { Time.now }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user) { FactoryBot.build(:user) }
+  
+  context 'when validates name' do
+    it 'should be present' do
+      user.name = nil
+      expect(user).to_not be_valid
+    end
+  end
+
+  context 'when validates email' do
+    it 'should be present' do
+      user.email = nil
+      expect(user).to_not be_valid
+    end
+  end
+
+  context 'when validates password' do
+    it 'should be present' do
+      user.password = nil
+      expect(user).to_not be_valid
+    end
+  end
 end
