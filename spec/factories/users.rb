55821c2a4ffe8b67651841f34ccf043b9f8d06aa
gabyse1 <gaby.es.iaa@gmail.com>
2022-05-31 06:34:57 -0500

    Configure the RSpec to only accept the new :expect syntax to test on new projects

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+# This will guess the User class
+FactoryBot.define do
+  factory :user do
+    name { "test user" }
+    email  { 'test.user@gmail.com' }
+    password  { 'password' }
+    password_confirmation  { 'password' }
+    confirmed_at { Time.now }
+  end
+end
