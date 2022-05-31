e93fff46005b93e0aa7426d9bf5d62eb6ba567dd
gabyse1 <gaby.es.iaa@gmail.com>
2022-05-31 08:02:59 -0500

    Create RecipeFood model associations

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,4 +1,4 @@
 class RecipeFood < ApplicationRecord
-  belongs_to :recipe
-  belongs_to :food
+  belongs_to :recipe, class_name: 'Recipe', foreign_key: 'recipe_id', inverse_of: :recipe_foods
+  belongs_to :food, class_name: 'Food', foreign_key: 'food_id', inverse_of: :recipe_foods
 end
