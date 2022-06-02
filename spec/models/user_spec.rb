9cb50b74fcdc912eb98c3396349406e6186744e0
gabyse1 <gaby.es.iaa@gmail.com>
2022-06-02 16:11:44 -0500

    Create the User model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,26 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:user) { build :user }
 
-  let(:user) { FactoryBot.build(:user) }
-  
-  context 'when validates name' do
+  context 'when validates, name' do
     it 'should be present' do
       user.name = nil
       expect(user).to_not be_valid
     end
   end
 
-  context 'when validates email' do
+  context 'when validates, email' do
     it 'should be present' do
       user.email = nil
       expect(user).to_not be_valid
     end
   end
 
-  context 'when validates password' do
+  context 'when validates, password' do
     it 'should be present' do
       user.password = nil
+      expect(user).to_not be_valid
+    end
+
+    it 'should have almost 6 characters' do
+      user.password = 'a' * 5
       expect(user).to_not be_valid
     end
   end
