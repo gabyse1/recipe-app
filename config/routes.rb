feb0d0e138a4730f27d3ce49c392103f4641bbc5
gabyse1 <gaby.es.iaa@gmail.com>
2022-06-05 13:04:56 -0500

    Add recipe pathas to the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :foods, only: %i[index new create destroy]
+  resources :recipes, only: %i[index new create show destroy]
 
   # Defines the root path route ("/")
-  root "foods#index"
+  root "recipes#index"
 end
