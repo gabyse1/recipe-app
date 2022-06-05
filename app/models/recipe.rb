ffc5a6a9636473d68787592c6415a1c7c88ec00e
gabyse1 <gaby.es.iaa@gmail.com>
2022-06-05 10:30:06 -0500

    Add attribute validation and default_values method to the Food model according to spec tests.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,17 @@
 class Recipe < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id', inverse_of: :recipes
   has_many :recipe_foods, class_name: 'RecipeFood', foreign_key: 'recipe_id', inverse_of: :recipe, dependent: :destroy
+
+  validates :name, presence: true
+  validates :preparation_time, presence: true, numericality: true, comparison: { greater_than: 0 }
+  validates :cooking_time, presence: true, numericality: true, comparison: { greater_than: 0 }
+  validates :description, presence: true
+
+  after_initialize :default_values
+
+  private
+
+  def default_values
+    self.public ||= false
+  end
 end
