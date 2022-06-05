b6ee5baba9ad169203e1c8384f85b5a2d70254b6
gabyse1 <gaby.es.iaa@gmail.com>
2022-06-05 06:25:08 -0500

    Create the Recipe model-type spec tests to validate its attributes.

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { build :user }
+  let(:recipe) { build :recipe, user: }
+
+  context 'when validates, name' do
+    it 'should be present' do
+      recipe.name = nil
+      expect(recipe).to_not be_valid
+    end
+  end
+
+  context 'when validates, preparation_time' do
+    it 'should be present' do
+      recipe.preparation_time = nil
+      expect(recipe).to_not be_valid
+    end
+
+    it 'should be a numericality value' do
+      recipe.preparation_time = 'aaa'
+      expect(recipe).to_not be_valid
+    end
+  end
+
+  context 'when validates, cooking_time' do
+    it 'should be present' do
+      recipe.cooking_time = nil
+      expect(recipe).to_not be_valid
+    end
+
+    it 'should be a numericality value' do
+      recipe.cooking_time = 'aaa'
+      expect(recipe).to_not be_valid
+    end
+  end
+
+  context 'when validates, description' do
+    it 'should be present' do
+      recipe.description = nil
+      expect(recipe).to_not be_valid
+    end
+  end
 end
